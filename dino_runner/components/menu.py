661fdfa465f759f0d15cc885b55eb001dd498b26
Fernando Marques <133921934+bigbrainandao@users.noreply.github.com>
2023-05-25 16:51:10 -0300

    Update menu.py
    atulizacao final

--- dino_runner/components/menu.py
+++ dino_runner/components/menu.py
@@ -3,7 +3,7 @@ import os
 
 from pygame.locals import *
 from dino_runner.utils.text_utils import draw_message_component
-from dino_runner.utils.constants import YELLOW, JUMPING, SCREEN_HEIGHT, SCREEN_WIDTH, IMG_DIR
+from dino_runner.utils.constants import YELLOW, JUMPING, SCREEN_HEIGHT, SCREEN_WIDTH, IMG_DIR, START_MENU, MENU_BLEEP
 
 MENU_IMG = JUMPING
 HALF_SCREEN_WIDTH = SCREEN_WIDTH // 2
@@ -20,7 +20,7 @@ BACK_GROUND = pygame.transform.scale(BACK_GROUND, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
 class Menu:
     def __init__(self):
-        self.options = ["start", "shop", "credits", "exit"]
+        self.options = ["start", "credits", "exit"]
         self.index = 0
         self.shop_index = 0
         self.shop_menu = False
@@ -49,18 +49,20 @@ class Menu:
 
     def exec_esc(self, game):
         if self.shop_menu:
+            MENU_BLEEP.play()
+            pygame.time.delay(250)
             self.shop_menu = False
         elif self.rank_menu:
             self.rank_menu = False
         else:
             self.game_quit(game)
     
-    def show_menu(self, game, screen, score, death_count, all_time_score, rank_menu):
+    def show_menu(self, game, screen, score, death_count, rank_menu):
         if not self.shop_menu and not self.rank_menu:
             self.draw_menu(screen, score, death_count)
             self.handle_events_on_menu(game)
         elif self.shop_menu:
-            self.draw_shop_menu(screen, all_time_score)
+            self.draw_shop_menu(screen)
             self.handle_events_on_shop_menu(game)
         elif self.rank_menu:
             self.draw_rank_menu(screen, rank_menu)
@@ -77,6 +79,7 @@ class Menu:
 
     def handle_events_on_shop_menu(self, game):
         for event in pygame.event.get():
+            MENU_BLEEP.play()
             if event.type == KEYDOWN:
                 if [K_SPACE, K_KP_ENTER, K_RETURN].__contains__(event.key):
                     option_selected = self.select()
@@ -97,14 +100,12 @@ class Menu:
                     option_selected = self.select()
                     if ["start", "restart"].__contains__(option_selected):
                         game.run()
-                    elif option_selected == "shop":
+                    elif option_selected == "credits":
                         self.shop_menu = True
                     elif option_selected == "exit":
                         self.game_quit(game)
                     elif option_selected == "rank":
                         self.rank_menu = True
-                    elif option_selected == "credits":
-                        option_selected == True
                 elif event.key == K_ESCAPE:
                     self.exec_esc(game)
                 elif event.key == K_UP or event.key == K_w:
@@ -174,6 +175,7 @@ class Menu:
     def draw_menu(self, screen, score, death_count):
         screen.blit(BACK_GROUND, (0, 0))
         height = screen.get_height()
+        screen.blit(START_MENU, (HALF_SCREEN_WIDTH - 150, height - 600))
         if death_count == 0:
             self.options = ["start", "credits", "exit"]
         else: 
@@ -188,12 +190,6 @@ class Menu:
                 screen,
                 pos_x_center = HALF_SCREEN_WIDTH + HALF_SCREEN_WIDTH / 2
             )
-
-        menu_img_width = MENU_IMG.get_width()
-        menu_img_height = MENU_IMG.get_height()
-        scaled_mult = 2
-        menu_img_scaled = pygame.transform.scale(MENU_IMG, (menu_img_width * scaled_mult, menu_img_height * scaled_mult))
-        screen.blit(menu_img_scaled, (10 + HALF_SCREEN_WIDTH - menu_img_scaled.get_width() / 2, HALF_SCREEN_HEIGHT - FONT_SIZE * 4 - MENU_IMG.get_height()))
 
         for index, option in enumerate(self.options):
             draw_message_component(
